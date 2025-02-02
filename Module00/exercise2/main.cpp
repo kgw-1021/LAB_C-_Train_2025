db2965808a9840c036bda4274fcddb13a3a70b5d
kgw-1021 <dagonkw@gmail.com>
2025-02-02 22:17:08 +0900

    Update Moduel00

--- Module00/exercise2/main.cpp
+++ Module00/exercise2/main.cpp
@@ -1,162 +1,6 @@
-
-#include "utils.hpp"
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <string>
+#include "phoneBook.hpp"
 
 using namespace std;
-
-struct phoneObject {
-    string name;
-    string number;
-    string nickname;
-    bool isBookmarked = false;
-};
-
-class PhoneBook {
-private:
-    vector<phoneObject> phoneList;
-
-public:
-    void ReadPhoneBook();
-    void SavePhoneBook();
-    void SearchPhoneBook();
-    void DisplayList();
-    void ModifyList(const string& action);
-    void addPhoneObject(const phoneObject& obj);
-};
-
-void PhoneBook::addPhoneObject(const phoneObject& obj) {
-    phoneList.push_back(obj);
-}
- 
-void PhoneBook::ReadPhoneBook() {
-    ifstream file("phoneBook.csv");
-    if (!file.is_open()) {
-        cout << "No existing phoneBook.csv file. Starting fresh." << endl;
-        return;
-    }
-
-    string line;
-    while (getline(file, line)) {
-        size_t pos1 = line.find(",");
-        size_t pos2 = line.find(",", pos1 + 1);
-        size_t pos3 = line.find(",", pos2 + 1);
-
-        if (pos1 != string::npos && pos2 != string::npos && pos3 != string::npos) {
-            phoneObject obj;
-            obj.name = line.substr(0, pos1);
-            obj.number = line.substr(pos1 + 1, pos2 - pos1 - 1);
-            obj.nickname = line.substr(pos2 + 1, pos3 - pos2 - 1);
-            obj.isBookmarked = line.substr(pos3 + 1) == "1";
-            phoneList.push_back(obj);
-        }
-    }
-
-    file.close();
-}
-
-void PhoneBook::SearchPhoneBook() {
-    cout << "Enter What you search : ";
-    string key;
-    cin >> key;
-    cout << "PhoneBook List:\n";
-    cout << "| # |  Name  |  Phone Number  |  Nick Name  |" << endl;
-    for (size_t i = 0; i < phoneList.size(); ++i) {
-        if (phoneList[i].name.find(key) != string::npos) {
-            cout << (i + 1) << ". "
-                << phoneList[i].name 
-                << phoneList[i].number
-                << phoneList[i].nickname 
-                << (phoneList[i].isBookmarked ? "★" : "")
-                << endl;
-        }
-    }
-    bool run = true;
-    cout << "Press any button to go Menu.";
-    int choice = getch();
-    switch (choice)
-    {
-    default:
-      break;
-    }
-    
-}
-
-void PhoneBook::SavePhoneBook() {
-    ofstream file("phoneBook.csv");
-    for (const auto& obj : phoneList) {
-        file << obj.name << "," << obj.number << "," << obj.nickname << "," << (obj.isBookmarked ? "1" : "0") << endl;
-    }
-    file.close();
-}
-
-void PhoneBook::DisplayList() {
-    cout << "PhoneBook List:\n";
-    cout << "| # |      Name      |      Phone Number      |      Nick Name      |" << endl;
-    for (size_t i = 0; i < phoneList.size(); ++i) {
-        cout << "  " 
-            << (i + 1) << ".      "
-            << phoneList[i].name << "       "
-            << phoneList[i].number << "           "
-            << phoneList[i].nickname  << "           "
-            << (phoneList[i].isBookmarked ? "★" : "")
-            << endl;
-    }
-}
-
-void PhoneBook::ModifyList(const string& action) {
-    if (phoneList.empty()) {
-        cout << "The phone book is empty.\n";
-        return;
-    }
-
-    size_t currentIndex = 0;
-    bool running = true;
-
-    while (running) {
-        system("clear");
-        DisplayList();
-        
-        cout << "\033[" << (currentIndex + 1) << ";1H> "; 
-
-        int key = getch();
-        switch (key) {
-        case 72: // Up key
-            if (currentIndex > 0)
-                --currentIndex;
-            break;
-        case 80: // Down key
-            if (currentIndex < phoneList.size() - 1)
-                ++currentIndex;
-            break;
-        case 13: // Enter key
-            if (action == "delete") {
-                phoneList.erase(phoneList.begin() + currentIndex);
-                cout << "Item deleted successfully!\n";
-            } else if (action == "bookmark") {
-                phoneList[currentIndex].isBookmarked = true;
-                cout << "Item bookmarked successfully!\n";
-            }
-            running = false;
-            break;
-        case 27: // ESC key
-            running = false;
-            break;
-        }
-    }
-}
-
-int Menu() {
-  int input = getch();
-  int notAllowedInput = 99;
-  if (input != '1' && input != '2' && input != '3' && input != '4' &&
-      input != '5')
-    return notAllowedInput;
-  else
-    return input;
-}
 
 int main() {
     PhoneBook phonebook;
@@ -165,6 +9,7 @@ int main() {
     bool run = true;
     system("clear");
     while (run) {
+        phonebook.SortPhoneBook();
           cout << " Please choose what to do!" << endl;
           cout << " | 1.Add Phone number  | 2.Search Phone number | 3.Remove Phone "
           "number | 4.Bookmark Phone number | 5.Exit Program |" 
@@ -175,11 +20,11 @@ int main() {
         case '1': {
             phoneObject obj;
             cout << "Enter Name: ";
-            cin >> obj.name;
+            getline(cin, obj.name);
             cout << "Enter Number: ";
-            cin >> obj.number;
+            getline(cin, obj.number);
             cout << "Enter Nickname: ";
-            cin >> obj.nickname;
+            getline(cin, obj.nickname);
             obj.isBookmarked = false;
             phonebook.addPhoneObject(obj);
             cout << "Phone number added successfully!" << endl;
@@ -205,7 +50,7 @@ int main() {
             break;
         default:
             system("clear");
-            cout << "Invalid input. Please try again." << endl;
+            cout << "Invalid input. Please try again." << endl << endl;
         }
     }
 
--- Module00/exercise2/phoneBook.hpp
+++ Module00/exercise2/phoneBook.hpp
@@ -1,61 +1,164 @@
-#include <string.h>
+#include "utils.hpp"
 #include <iostream>
-#include <stdlib.h>
 #include <fstream>
+#include <iomanip>
+#include <algorithm>
 #include <vector>
+#include <string>
+#include <signal.h>
 
 using namespace std;
 
 struct phoneObject {
-  string name;
-  string number;
-  string nickname;
+    string name;
+    string number;
+    string nickname;
+    bool isBookmarked = false;
 };
 
 class PhoneBook {
-  private:
-    phoneObject phoneobj;
-    vector <phoneObject> phoneList;
+private:
+    vector<phoneObject> phoneList;
 
-  public:
-    
-    void PhonenumberADD();
-
-    void PhoneNumberSearch();
-
-    void phoneNumberRemove();
-
-    void phoneNumberBookmark();
-
+public:
     void ReadPhoneBook();
-
-    void get_List();
-
+    void SavePhoneBook();
+    void SearchPhoneBook();
+    void SortPhoneBook();
+    void DisplayList();
+    void ModifyList(const string& action);
+    void addPhoneObject(const phoneObject& obj);
 };
 
-void PhoneBook::PhonenumberADD(){
-  cout << "ADD" << endl;
+void PhoneBook::addPhoneObject(const phoneObject& obj) {
+    phoneList.push_back(obj);
+}
+ 
+void PhoneBook::ReadPhoneBook() {
+    ifstream file("phoneBook.csv");
+    if (!file.is_open()) {
+        cout << "No existing phoneBook.csv file. Starting fresh." << endl;
+        ofstream newFile("phoneBook.csv");
+        newFile.close();
+        return;
+    }
+
+    string line;
+    while (getline(file, line)) {
+        size_t pos1 = line.find(",");
+        size_t pos2 = line.find(",", pos1 + 1);
+        size_t pos3 = line.find(",", pos2 + 1);
+
+        if (pos1 != string::npos && pos2 != string::npos && pos3 != string::npos) {
+            phoneObject obj;
+            obj.name = line.substr(0, pos1);
+            obj.number = line.substr(pos1 + 1, pos2 - pos1 - 1);
+            obj.nickname = line.substr(pos2 + 1, pos3 - pos2 - 1);
+            obj.isBookmarked = line.substr(pos3 + 1) == "1";
+            phoneList.push_back(obj);
+        }
+    }
+
+    file.close();
 }
 
-void PhoneBook::PhoneNumberSearch(){
-  cout << "Search" << endl;
+void PhoneBook::SearchPhoneBook() {
+    cout << "Enter What you search : ";
+    string key;
+    getline(cin, key);
+    system("clear");
+    cout << "Search Result :" << endl;
+    cout << "|  #  |      Name      |    Phone Number    |    Nick Name    |  Book Mark  |" << endl;
+    for (size_t i = 0; i < phoneList.size(); ++i) {
+        if (phoneList[i].name.find(key) != string::npos) {
+            cout << "| " << setw(2) << right << (i + 1) << ". " << "| "
+                << setw(15) << left << phoneList[i].name << "| "
+                << setw(19) << left << phoneList[i].number << "| "
+                << setw(16) << left << phoneList[i].nickname  << "|      "
+                << (phoneList[i].isBookmarked ? "★" : " ") << "      |"
+                << endl;
+        }
+    }
+    bool run = true;
+
+    cout << "Press ESC to go Menu.";    
+    while (run) {
+
+      int choice = getch();
+
+      switch (choice)
+      {
+      case 27:    // ESC -> 27 (ASCII)
+        run = false;
+        break;
+      default:
+        break;
+      }
+    }
 }
 
-void PhoneBook::phoneNumberRemove(){
-  cout << "Remove" << endl;
-}  
-
-void PhoneBook::phoneNumberBookmark(){
-  cout << "BookMark" << endl;
+void PhoneBook::SortPhoneBook() {
+    sort(phoneList.begin(), phoneList.end(), [](const phoneObject &a, const phoneObject &b) {
+        return a.isBookmarked > b.isBookmarked;
+    });
 }
 
-void PhoneBook::ReadPhoneBook(){
-  cout << "Read " << endl;
-  ifstream file("phoneBook.csv")
-
-  getline(file, line);
-
+void PhoneBook::SavePhoneBook() {
+    ofstream file("phoneBook.csv");
+    for (const auto& obj : phoneList) {
+        file << obj.name << "," << obj.number << "," << obj.nickname << "," << (obj.isBookmarked ? "1" : "0") << endl;
+    }
+    file.close();
 }
 
-void PhoneBook::get_List() {
+void PhoneBook::DisplayList() {
+    cout << "PhoneBook List :" << endl;
+    cout << "|  #  |      Name      |      Phone Number      |    Nick Name    |  Book Mark  |" << endl;
+    for (size_t i = 0; i < phoneList.size(); ++i) {
+        cout << "| " << setw(2) << right << (i + 1) << ". " << "| "
+            << setw(15) << left << phoneList[i].name << "| "
+            << setw(23) << left << phoneList[i].number << "| "
+            << setw(16) << left << phoneList[i].nickname  << "|      "
+            << (phoneList[i].isBookmarked ? "★" : " ") << "      |"
+            << endl;
+    }
+}
+
+void PhoneBook::ModifyList(const string& action) {
+    system("clear");
+    if (phoneList.empty()) {
+        cout << "The phone book is empty." << endl;
+        return;
+    }
+
+    DisplayList();
+    cout << "Enter the number to " << action << ": ";
+    int index;
+    cin >> index;
+    cin.ignore();
+
+    if (index < 1 || index > static_cast<int>(phoneList.size())) {
+        cout << "Invalid number. Returning to menu." << endl;
+        return;
+    }
+
+    --index;
+
+    if (action == "delete") {
+        phoneList.erase(phoneList.begin() + index);
+        cout << "Phone Number deleted successfully!\n";
+    } else if (action == "bookmark") {
+        phoneList[index].isBookmarked = true;
+        cout << "Phone Number bookmarked successfully!\n";
+    }
+}
+
+int Menu() {
+  int input = getch();
+  int notAllowedInput = 99;
+  if (input != '1' && input != '2' && input != '3' && input != '4' &&
+      input != '5')
+    return notAllowedInput;
+  else
+    return input;
 }
